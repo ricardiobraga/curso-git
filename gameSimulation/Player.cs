08d8fe2d0f33e283f8aee76f6ec9521e7c54e64a
Ricardo Braga <rcardiobraga@gmail.com>
2020-07-15 20:17:58 -0300

    Base da ficha de personagem

---
+++ gameSimulation/Player.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gameSimulation
+{
+    class Player
+    {
+
+        public string Name { get; set; }
+        public string PlayerClass { get; set; }
+        public int Life { get; set; }
+        public int Dextery { get; set; }
+        public int Strength { get; set; }
+        public int Wisdom { get; set; }
+
+        public Player(string name)
+        {
+
+            Name = name;
+            
+            
+        }
+
+        public void PClass(int classC)
+        {
+            if (classC == 1)
+            {
+                
+                PlayerClass = "Ranger";
+                Dextery = RollDice(1, 6) + 8; ;
+                Strength = RollDice(1, 6) + 6;
+                Wisdom = RollDice(1,6) + 6;
+
+                
+            }
+            else if (classC == 2)
+            {
+                PlayerClass = "Warrior";
+                Dextery = RollDice(1, 6) + 6;
+                Strength = RollDice(1, 6 + 6);
+                Wisdom = RollDice(1, 6) + 8;
+                
+            }
+            else if (classC == 3)
+            {
+                PlayerClass = "Mage";
+                Dextery = RollDice(1, 6) + 6;
+                Strength = RollDice(1, 6) + 6;
+                Wisdom = RollDice(1, 6)+ 8;
+                
+            }          
+
+            
+
+
+        }
+
+        public void LifeCalc()
+        {
+            var rand = new Random();
+            Life = 12 + rand.Next(1, 12);
+
+        }
+
+        public int RollDice(int rolls, int dice)
+        {
+            var rand = new Random();
+            return rolls * rand.Next(1, dice);
+        }
+
+        public override string ToString()
+        {
+            return "Dados do Personagem criado: "
+                    + "Nome: "
+                    + Name
+                    + " Classe: "
+                    + PlayerClass
+                    + " Vida: "
+                    + Life
+                    + " Dex: "
+                    + Dextery
+                    + " Str: "
+                    + Strength
+                    + " Wis: "
+                    + Wisdom;
+        }
+
+
+
+
+    }
+}
---
+++ gameSimulation/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace gameSimulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.SetBufferSize(1600,900);
+
+            Console.WriteLine("Criação do personagem: ");
+            Console.WriteLine();
+            Console.Write("Digite o Nome do personagem: ");
+            string name = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Escolha uma classe para o personagem: ");
+            Console.Write("Digite (1) para Ranger, (2) para Warrior ou (3) para Mago: ");
+            int classC = int.Parse(Console.ReadLine());
+
+            
+
+            
+            
+
+            Player p1 = new Player (name);
+
+            p1.LifeCalc();
+            p1.PClass(classC);            
+
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(p1);
+            Console.WriteLine();
+
+            
+
+           
+            
+            
+            
+
+;
+            
+
+
+
+            
+
+
+
+ 
+
+
+
+        }
+    }
+}

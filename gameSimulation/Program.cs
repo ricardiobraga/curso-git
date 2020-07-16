ab95426fe3f5f85ffeffa31e72d798363b3770a6
Ricardo Braga <rcardiobraga@gmail.com>
2020-07-15 21:00:59 -0300

    ajuste de formatação

--- gameSimulation/Program.cs
+++ gameSimulation/Program.cs
@@ -6,8 +6,7 @@ namespace gameSimulation
     {
         static void Main(string[] args)
         {
-
-            Console.SetBufferSize(1600,900);
+            Console.SetBufferSize(1600, 900);
 
             Console.WriteLine("Criação do personagem: ");
             Console.WriteLine();
@@ -18,41 +17,16 @@ namespace gameSimulation
             Console.Write("Digite (1) para Ranger, (2) para Warrior ou (3) para Mago: ");
             int classC = int.Parse(Console.ReadLine());
 
-            
-
-            
-            
-
-            Player p1 = new Player (name);
+            Player p1 = new Player(name);
 
             p1.LifeCalc();
-            p1.PClass(classC);            
+            p1.PClass(classC);
 
             Console.WriteLine();
 
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(p1);
             Console.WriteLine();
-
-            
-
-           
-            
-            
-            
-
-;
-            
-
-
-
-            
-
-
-
- 
-
-
 
         }
     }
